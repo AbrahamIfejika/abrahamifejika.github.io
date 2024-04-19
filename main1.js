f9183b72573ea442174495269d8be329e6671afd
AbrahamIfejika <158232688+AbrahamIfejika@users.noreply.github.com>
2024-04-19 19:03:51 -0400

    assignment 4 part 2 - third commit

--- main1.js
+++ main1.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imageAltText = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Closeup of the inside of a canyon',
+    'pic3.jpg': 'Closeup of purple flowers',
+    'pic4.jpg': 'Closeup of an Egyptian tomb painiting',
+    'pic5.jpg': 'Closeup of a butterfly on a leaf'
+};
 
 /* Looping through images */
 
@@ -16,3 +24,4 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+
