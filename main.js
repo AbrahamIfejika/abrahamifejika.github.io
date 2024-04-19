007107f9d442f7a1b160b12701f0bee61168df4a
AbrahamIfejika <158232688+AbrahamIfejika@users.noreply.github.com>
2024-04-19 17:31:05 -0400

    assignment 4 part 1 - fourth commit

--- main.js
+++ main.js
@@ -15,10 +15,20 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
+    function result() {
+        let newStory = storyText;
+      
+        const xItem = randomValueFromArray(insertX);
+        const yItem = randomValueFromArray(insertY);
+        const zItem = randomValueFromArray(insertZ);
+      
+        newStory = newStory.replaceAll(':insertx:', xItem);
+        newStory = newStory.replaceAll(':inserty:', yItem);
+        newStory = newStory.replaceAll(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
